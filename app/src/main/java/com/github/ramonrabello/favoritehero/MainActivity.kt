9cf8fe91ae02ce99e4f1dc20617f8dac669e503a
ramon.rabello <ramon.rabello@gmail.com>
2018-07-12 15:03:28 -0300

    adding activity transitions support.

--- app/src/main/java/com/github/ramonrabello/favoritehero/MainActivity.kt
+++ app/src/main/java/com/github/ramonrabello/favoritehero/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.github.ramonrabello.favoritehero
 
+import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.transition.Explode
+import android.view.Window
 import com.github.ramonrabello.favoritehero.favorites.FavoriteHeroListFragment
 import com.github.ramonrabello.favoritehero.heroes.HeroListFragment
 import dagger.android.AndroidInjection
@@ -13,11 +16,6 @@ import dagger.android.support.HasSupportFragmentInjector
 import kotlinx.android.synthetic.main.activity_main.navigation
 import kotlinx.android.synthetic.main.activity_main.toolbar
 import javax.inject.Inject
-import android.app.SearchManager
-import android.content.Context
-import android.support.v4.view.MenuItemCompat
-import android.support.v7.widget.SearchView
-import android.view.Menu
 
 
 /**
@@ -30,12 +28,18 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_heros -> {
-                supportFragmentManager.beginTransaction().replace(R.id.fragment_content, HeroListFragment.newInstance()).commit()
+                supportFragmentManager.beginTransaction()
+                        .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                        .replace(R.id.fragment_content, HeroListFragment.newInstance())
+                        .commit()
                 toolbar.title = getString(R.string.title_heros)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_favorites -> {
-                supportFragmentManager.beginTransaction().replace(R.id.fragment_content, FavoriteHeroListFragment.newInstance()).commit()
+                supportFragmentManager.beginTransaction()
+                        .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                        .replace(R.id.fragment_content, FavoriteHeroListFragment.newInstance())
+                        .commit()
                 toolbar.title = getString(R.string.title_favorites)
                 return@OnNavigationItemSelectedListener true
             }
@@ -46,6 +50,11 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+            window.enterTransition = Explode()
+            window.exitTransition = Explode()
+        }
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
