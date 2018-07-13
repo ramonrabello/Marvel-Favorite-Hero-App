defcdf89058e84008ecee4910b6bb226007573e0
ramon.rabello <ramon.rabello@gmail.com>
2018-07-13 03:02:56 -0300

    optimizing imports.

--- app/src/androidTest/java/com/github/ramonrabello/favoritehero/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/ramonrabello/favoritehero/MainActivityTest.kt
@@ -6,21 +6,18 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.espresso.matcher.ViewMatchers.withParent
-import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.filters.SmallTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import android.support.v7.widget.Toolbar
+import com.github.ramonrabello.favoritehero.util.matchers.ToolbarMatchers.withToolbarTitle
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom
-import android.support.v7.widget.Toolbar
-import com.github.ramonrabello.favoritehero.util.matchers.ToolbarMatchers.withToolbarTitle
 
 
 /**
